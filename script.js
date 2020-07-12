21fd7764e34e71787e1a33908c1f3bfc47533ed1
lizasilent <elizavetad@yandex.ru>
2020-07-12 22:58:45 +0300

    правки по ревью 92 из 100

--- script.js
+++ script.js
@@ -4,29 +4,28 @@ let closeModalButton = document.querySelector(".popup__close-btn");
 let popup = modal.querySelector(".popup__container");
 let userName = document.querySelector(".profile__name");
 let userInformation = document.querySelector(".profile__description");
-let inputName = modal.querySelector(".popup__name");
-let inputDescription = modal.querySelector(".popup__description");
+let inputName = modal.querySelector(".popup__text_name");
+let inputDescription = modal.querySelector(".popup__text_description");
 
 openModalButton.addEventListener("click", () => {
 
-    modal.classList.add("popup_is-open");
+modal.classList.add("popup_is-open");
 })
 
 closeModalButton.addEventListener('click', () => {
 
-    modal.classList.remove("popup_is-open");
+modal.classList.remove("popup_is-open");
 
-     inputName.value = userName.textContent
-     inputDescription.value = userInformation.textContent
+inputName.value = userName.textContent
+inputDescription.value = userInformation.textContent
 
 })
 
 popup.addEventListener("submit", (event) => {
 
-    userName.textContent = inputName.value
-    userInformation.textContent = inputDescription.value
-    event.preventDefault();
+userName.textContent = inputName.value
+userInformation.textContent = inputDescription.value
+event.preventDefault();
 
-    modal.classList.remove("popup__is-open");
-})
-
+modal.classList.remove("popup_is-open");
+}) 

0ab60e4f16bb69523ba92828702744566a076d94
Liza_silent <elizavetad@ya.ru>
2020-07-13 16:08:05 +0300

    поправки по ревью

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ let userInformation = document.querySelector(".profile__description");
 let inputName = modal.querySelector(".popup__text_name");
 let inputDescription = modal.querySelector(".popup__text_description");
 
-let openPopup = (
-openModalButton.addEventListener("click", () => {
-modal.classList.add("popup_is-open");
-inputName.value = userName.textContent;
-inputDescription.value = userInformation.textContent;
-}))
+function openPopup () {
+  modal.classList.add("popup_is-open");
+  inputName.value = userName.textContent;
+  inputDescription.value = userInformation.textContent;
+}
 
-function hidePopup() {
-modal.classList.remove("popup_is-open")};
+function closePopup () {
+  modal.classList.remove("popup_is-open");
+  }
 
-let closePopup = (
-closeModalButton.addEventListener('click', () => {
-hidePopup();
-}))
+function saveInfo () {
+  event.preventDefault();
+  userName.textContent = inputName.value;
+  userInformation.textContent = inputDescription.value;
+  closePopup ();
+  }
 
-let saveInfo = (
-popup.addEventListener("submit", (event) => {
-event.preventDefault();
-userName.textContent = inputName.value;
-userInformation.textContent = inputDescription.value;
-hidePopup();
-}))
+openModalButton.addEventListener("click", openPopup);
+closeModalButton.addEventListener('click', closePopup);
+popup.addEventListener("submit", saveInfo);
+
+

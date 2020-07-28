9840387373bf7999057e6076fe5dae3c5bf650f2
lizasilent <elizavetad@yandex.ru>
2020-07-28 15:29:23 +0300

    работа 5\10

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-let openModalButton = document.querySelector(".profile__edit-btn");
-let modal = document.querySelector(".popup");
-let closeModalButton = document.querySelector(".popup__close-btn");
-let popup = modal.querySelector(".popup__container");
+//Modals 
+
+let editProfileModal = document.querySelector(".popup");
+let addCardsModal = document.querySelector(".popup_type_add-cards");
+let popup = editProfileModal.querySelector(".popup__container");
+
+//Data etc
 let userName = document.querySelector(".profile__name");
 let userInformation = document.querySelector(".profile__description");
-let inputName = modal.querySelector(".popup__text_name");
-let inputDescription = modal.querySelector(".popup__text_description");
+let inputName = editProfileModal.querySelector(".popup__text_name");
+let inputDescription = editProfileModal.querySelector(".popup__text_description");
+
+//Buttons
+let addCardButton = document.querySelector('.profile__add-btn');
+let likeCardButton = document.querySelector(".grid__like-btn");
+let editProfileButton = document.querySelector(".profile__edit-btn");
+let closeModalButton = document.querySelector(".popup__close-btn");
+
+
 
 function openPopup () {
-  modal.classList.add("popup_is-open");
+  editProfileModal.classList.toggle("popup_is-open");
   inputName.value = userName.textContent;
   inputDescription.value = userInformation.textContent;
 }
 
 function closePopup () {
-  modal.classList.remove("popup_is-open");
+  editProfileModal.classList.remove("popup_is-open");
   }
+
+
+function openPopup2 () {
+  addCardsModal.classList.toggle("popup_is-open");
+ 
+}
+
+function closePopup2 () {
+  addCardsModal.classList.remove("popup_is-open");
+  
+    } 
+
 
 function saveInfo (event) {
   event.preventDefault();
   userName.textContent = inputName.value;
   userInformation.textContent = inputDescription.value;
-  closePopup ();
+  closePopup();
   }
 
-openModalButton.addEventListener("click", openPopup);
+editProfileButton.addEventListener("click", openPopup);
 closeModalButton.addEventListener('click', closePopup);
 popup.addEventListener("submit", saveInfo);
+addCardButton.addEventListener("click", openPopup2);
+addCardsModal.addEventListener('click', closePopup2);
 
 
 const initialCards = [
@@ -66,13 +91,20 @@ initialCards.forEach((data) => {
   const gridLikeButton = gridElement.querySelector(".grid__like-btn");
   const gridDeleteButton = gridElement.querySelector(".grid__delete-btn");
 
+  // gridLikeButton.addEventListener("click", handler);
+  // gridDeleteButton.addEventListener("click", handler);
+  // gridImage.addEventListener("click", handler);
+
   gridText.textContent = data.name;
   gridImage.src = data.link;
 
   const list = document.querySelector(".grid__template");
   list.prepend(gridElement);
-
-
-
 })
+
+// likeCardButton.addEventListener("click", (event) => {
+//   event.target.classList.toggle("grid__like_active-btn")
+// });
+
+
 

cc25c96a49edbf45b9964e45ec60a5909c874ef3
lizasilent <elizavetad@yandex.ru>
2020-07-29 15:44:03 +0300

    сделаны лайки и удаление карточки

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let editProfileModal = document.querySelector(".popup_type_edit-profile");
 let addCardsModal = document.querySelector(".popup_type_add-cards");
+let gridImageModal = document.querySelector(".popup_type_grid-img")
 let popup = editProfileModal.querySelector(".popup__container");
 
 //Data etc
@@ -13,10 +14,11 @@ let inputPlace = addCardsModal.querySelector(".popup__text_place");
 let inputImageSource = addCardsModal.querySelector(".popup__source");
 let gridTitle = document.querySelector(".grid__text");
 let gridImage = document.querySelector(".grid__image");
+let list = document.querySelector(".grid__template");
+
 
 //Buttons
 let addCardButton = document.querySelector('.profile__add-btn');
-let likeCardButton = document.querySelector(".grid__like-btn");
 let editProfileButton = document.querySelector(".profile__edit-btn");
 let closeEditProfileModalButton = editProfileModal.querySelector(".popup__close-btn");
 let closeAddCardsModalModalButton = addCardsModal.querySelector(".popup__close-btn");
@@ -34,17 +36,13 @@ function closePopup () {
   editProfileModal.classList.remove("popup_is-open");
   }
 
-
 function openPopup2 () {
   addCardsModal.classList.toggle("popup_is-open");
- 
 }
 
 function closePopup2 () {
   addCardsModal.classList.remove("popup_is-open");
-  
-    } 
-
+  }
 
 function saveInfo (event) {
   event.preventDefault();
@@ -53,11 +51,11 @@ function saveInfo (event) {
   closePopup();
   }
 
-  function addCardSubmitHandler (event) {
+function addCardSubmitHandler (event) {
     event.preventDefault();
     renderCard({name: inputPlace.value, link:inputImageSource.value});
     closePopup2();
-    }
+  }
   
 
 editProfileButton.addEventListener("click", openPopup);
@@ -65,7 +63,6 @@ closeEditProfileModalButton.addEventListener('click', closePopup);
 addCardButton.addEventListener("click", openPopup2);
 closeAddCardsModalModalButton.addEventListener('click', closePopup2);
 popup.addEventListener("submit", saveInfo);
-
 addCardsModal.addEventListener("submit", addCardSubmitHandler);
 
 
@@ -97,17 +94,15 @@ const initialCards = [
 ];
 
 const cardTemplate = document.querySelector(".template-card").content.querySelector(".grid__item");
-const list = document.querySelector(".grid__template");
+
+
 
 function createCard(data) {
   const gridElement = cardTemplate.cloneNode(true);
   const gridText = gridElement.querySelector(".grid__text");
   const gridImage = gridElement.querySelector(".grid__image");
-  
-  
   gridText.textContent = data.name;
   gridImage.src = data.link;
-
   return gridElement;
 }
 
@@ -115,21 +110,46 @@ function renderCard(data) {
   list.prepend(createCard(data)); 
 }
 
-
 initialCards.forEach((data) => {
   renderCard(data);
 })
 
 
-const gridLikeButton = gridElement.querySelector(".grid__like-btn");
-const gridDeleteButton = gridElement.querySelector(".grid__delete-btn");
+//Лайк в карточке 
+
+let gridLikeButton = document.querySelector(".grid__like-btn");
+
+function handleLikeClick() {
+  gridLikeButton.classList.toggle("grid__like_active-btn");
+  console.log(123); 
+}
+gridLikeButton.addEventListener("click", handleLikeClick);
 
 
-  // gridLikeButton.addEventListener("click", handler);
-  // gridDeleteButton.addEventListener("click", handler);
-  // gridImage.addEventListener("click", handler);
+// Удалить карточку
 
+let gridDeleteButton = document.querySelector(".grid__delete-btn");
+
+function handleDeleteClick() {
+  event.target.closest(".grid__item").remove();
+}
+  gridDeleteButton.addEventListener("click", handleDeleteClick);
   
-// likeCardButton.addEventListener("click", (event) => {
-//   event.target.classList.toggle("grid__like_active-btn")
-// });
+
+// Картинка 
+
+
+
+function handleImageClick() {
+  
+  gridImage.src = data.link;
+
+}
+
+// gridImage.addEventListener("click", handleImageClick);
+  
+
+
+ 
+
+ 

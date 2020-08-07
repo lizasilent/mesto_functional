6f696e1d75ddb5210746c6cec49d2559b18ffac9
lizasilent <elizavetad@yandex.ru>
2020-08-08 00:32:21 +0300

    нач

--- validation.js
+++ validation.js
@@ -1,17 +1,34 @@
 const object = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-btn',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit-btn",
+  inactiveButtonClass: ".popup__submit-btn_disabled",
+  inputErrorClass: ".form-error",
+  errorClass: "popup__error_visible",
 };
 
+const enableValidation = ({
+  formSelector,
+  inputSelector,
+  submitButtonSelector,
+  inactiveButtonClass,
+  inputErrorClass,
+  errorClass,
+}) => {
+  const forms = Array.from(document.querySelectorAll(formSelector));
+  forms.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    const inputs = formElement.querySelectorAll(inputSelector);
+    const buttonSubmit = formElement.querySelectorAll(submitButtonSelector);
 
-
-const enableValidation = ({ formSelector,  inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass
-}) =>
-{const form = Array.from(document.querySelectorAll(formSelector));
-  form.forEach()
+    inputs.forEach((inputElement) => {
+      inputElement.addEventListener("input", (evt) => {
+        console.log(inputElement.validity)
+      });
+    });
+  });
 };
 
+enableValidation(object);
